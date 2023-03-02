2169a5a117acc83b9347788d0272a8207a667f02
harrierpigeon <32232925+HarrierPigeon@users.noreply.github.com>
2023-03-02 16:48:16 -0600

    added WYLO.  May need to go fix the deploy script and make it not overwrite appscript.json

--- main.ts
+++ main.ts
@@ -2,6 +2,9 @@
 
 // do things, eventually
 
+//WYLO
+// currently following this guide: https://developers.google.com/looker-studio/connector/build
+// not sure exactly what the data format is supposed to look like yet.
 
 const cc = DataStudioApp.createCommunityConnector();
 
