0b2c81ae79949428928dd012282c21f22e188cad
harrierpigeon <32232925+HarrierPigeon@users.noreply.github.com>
2023-03-02 16:45:14 -0600

    TBH I have no idea how long this is going to take to build...

---
+++ looker-types.ts
@@ -0,0 +1,43 @@
+// request information provided by Looker studio.
+interface getDataRequest {
+    "configParams": object, // includes whatever stuff you asked for in getConfig
+    "scriptParams": {
+        "sampleExtraction": boolean,
+        "lastRefresh": string;
+    },
+    "dateRange": {
+        "startDate": string,
+        "endDate": string;
+    },
+    "fields": [
+        {
+            "name": string;
+        }
+    ],
+    "dimensionsFilters": [
+        [{
+            "fieldName": string,
+            "values": string[],
+            "type": DimensionsFilterType,
+            "operator": FilterOperator;
+        }]
+    ];
+}
+
+
+
+type DimensionsFilterType = "INCLUDE"|"EXCLUDE"
+
+enum FilterOperator {
+"EQUALS"="EQUALS",
+"CONTAINS"="CONTAINS",
+"REGEXP_PARTIAL_MATCH"="REGEXP_PARTIAL_MATCH",
+"REGEXP_EXACT_MATCH"="REGEXP_EXACT_MATCH",
+"IN_LIST"="IN_LIST",
+"IS_NULL"="IS_NULL",
+"BETWEEN"="BETWEEN",
+"NUMERIC_GREATER_THAN"="NUMERIC_GREATER_THAN",
+"NUMERIC_GREATER_THAN_OR_EQUAL"="NUMERIC_GREATER_THAN_OR_EQUAL",
+"NUMERIC_LESS_THAN"="NUMERIC_LESS_THAN",
+"NUMERIC_LESS_THAN_OR_EQUAL"="NUMERIC_LESS_THAN_OR_EQUAL",
+}
--- main.ts
+++ main.ts
@@ -5,20 +5,62 @@
 
 const cc = DataStudioApp.createCommunityConnector();
 
+/*
+    These are the functions Looker Studio itself calls:
+    getAuthType sets up authentication (I think this might need some changes to use Sheets?)
+
+    getConfig seems to be how to pass data through
+
+*/
+
 function getAuthType() {
     const AuthTypes = cc.AuthType;
     return cc.newAuthTypeResponse().setAuthType(AuthTypes.NONE).build();
 }
 
-function test() {
-    console.log(getAuthType);
-}
 
 function getConfig() {
+    // https://developers.google.com/looker-studio/connector/reference#configtype
     const config = cc.getConfig();
 
     config.newInfo().setId('sheetId').setText("Enter Sheet ID");
     config.newTextInput().setId("package").setName("Please Enter a sheet id").setHelpText("I dunno what I'm doing").setPlaceholder("aaaaaa");
 
     return config.build();
+}
+
+// interface schemaReturn {
+    
+// }
+
+function getSchema(request) {
+    // https://developers.google.com/looker-studio/connector/reference#getschema
+    
+    return {
+        schema:getFields().build()
+    }
+
+}
+
+
+function getData(request:getDataRequest) {
+    
+}
+
+// this function here is the one we'll modify with our own code in the future.
+function getFields() {
+    const fields = cc.getFields()
+    const types = cc.FieldType
+    const aggregations = cc.AggregationType;
+
+    fields.newDimension().setId("kiDate").setName("KI Date").setType(types.YEAR_MONTH_DAY)
+    fields.newDimension().setId("np").setName("New People").setType(types.NUMBER)
+    
+    return fields
+
+}
+
+// 
+function test() {
+    console.log(getAuthType);
 }
