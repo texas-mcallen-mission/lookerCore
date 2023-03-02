c336f656e022b2747452469db9be3649859ecadd
harrierpigeon <32232925+HarrierPigeon@users.noreply.github.com>
2023-03-02 16:13:46 -0600

    getting started.

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
 
 
 // do things, eventually
+
+
+const cc = DataStudioApp.createCommunityConnector();
+
+function getAuthType() {
+    const AuthTypes = cc.AuthType;
+    return cc.newAuthTypeResponse().setAuthType(AuthTypes.NONE).build();
+}
+
+function test() {
+    console.log(getAuthType);
+}
+
+function getConfig() {
+    const config = cc.getConfig();
+
+    config.newInfo().setId('sheetId').setText("Enter Sheet ID");
+    config.newTextInput().setId("package").setName("Please Enter a sheet id").setHelpText("I dunno what I'm doing").setPlaceholder("aaaaaa");
+
+    return config.build();
+}
